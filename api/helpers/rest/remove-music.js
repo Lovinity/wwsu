59f35b80dcc68392cdc990f0f4c5f06017295161
Patrick Schmalstig <xanaftp@gmail.com>
2018-08-08 03:47:33 -0400

    Re-define how removeMusic works
    Signed-off-by: Patrick Schmalstig <xanaftp@gmail.com>

--- api/helpers/rest/remove-music.js
+++ api/helpers/rest/remove-music.js
@@ -22,50 +22,41 @@ module.exports = {
     fn: async function (inputs, exits) {
         // API NOTE: This should never throw an error unless we have to stop all execution.
         sails.log.debug('Helper rest.removeMusic called.');
-        var removetrack = async function (cb) {
-            try {
-                // Get current queue
-                var queue = await sails.helpers.rest.getQueue();
-                var terminateloop = false;
-                var loopposition = 1;
+        try {
+            // First, get a snapshot of the current queue.
+            var queue = await sails.helpers.rest.getQueue();
 
-                // Loop through all the tracks in the queue
-                while (!terminateloop && loopposition < queue.length)
+            // Then, remove the entire queue.
+            await sails.helpers.rest.cmd("ClearPlaylist");
+
+            // Next, filter out all the non-music tracks out of the array, using a reverse for loop
+            for (i = queue.length - 1; i >= 0; i -= 1) {
+                if (parseInt(queue[i].ID) !== 0 && ((inputs.general && sails.config.custom.subcats.noClearGeneral.indexOf(parseInt(queue[i].IDSubcat)) === -1) || (!inputs.general && sails.config.custom.subcats.noClearShow.indexOf(parseInt(queue[i].IDSubcat)) === -1)))
                 {
-                    sails.log.verbose(`Trying ${loopposition}`);
-                    sails.log.verbose(`TrackType: ${queue[loopposition].TrackType}`);
-                    sails.log.verbose(`Elapsed: ${queue[loopposition].Elapsed}`);
-                    sails.log.verbose(`ID: ${queue[loopposition].ID}`);
-                    // If the track is not in a noClear category, remove it
-                    if (parseInt(queue[loopposition].ID) !== 0 && ((inputs.general && sails.config.custom.subcats.noClearGeneral.indexOf(parseInt(queue[loopposition].IDSubcat)) === -1) || (!inputs.general && sails.config.custom.subcats.noClearShow.indexOf(parseInt(queue[loopposition].IDSubcat)) === -1)))
+                    // If it was requested to keep track requests in the queue, skip over any tracks that were requested.
+                    if (!inputs.keepRequests || Requests.pending.indexOf(parseInt(queue[i].ID)) === -1)
                     {
-                        sails.log.verbose(`CLEAR`);
-                        // If it was requested to keep track requests in the queue, skip over any tracks that were requested.
-                        if (!inputs.keepRequests || Requests.pending.indexOf(parseInt(queue[loopposition].ID)) === -1)
-                        {
-                            sails.log.verbose(`REMOVING`);
-                            terminateloop = true;
-                            await sails.helpers.rest.cmd('RemovePlaylistTrack', loopposition - 1);
-
-                            // We have to re-execute the entire function again after each time we remove something so we can load the new queue in memory and have correct position numbers.
-                            setTimeout(async function () {
-                                await removetrack(cb);
-                            }, 100);
-                        }
+                        sails.log.verbose(`REMOVING`);
+                        queue.splice(i, 1);
                     }
-                    loopposition += 1;
                 }
-                if (!terminateloop)
-                    cb();
-            } catch (e) {
-                sails.log.error(e);
-                throw e;
             }
-        };
-        await removetrack(exits.success)
-                .catch(e => {
-                    return exits.error(e);
+
+            // Finally, re-queue the remaining tracks
+            if (queue.length > 0)
+            {
+                await sails.helpers.asyncForEach(queue, function (track, index) {
+                    return new Promise(async (resolve, reject) => {
+                        await sails.helpers.rest.cmd("LoadTrackToBottom", track.ID);
+                        return resolve(false);
+                    });
                 });
+            }
+
+            return exits.success();
+        } catch (e) {
+            return exits.success();
+        }
     }
 
 
