f093f780cd5f5d30ba4a2faa506e4bc8c6c87db6
Patrick Schmalstig <xanaftp@gmail.com>
2018-11-27 19:06:25 -0500

    Internal sign tweak
    Signed-off-by: Patrick Schmalstig <xanaftp@gmail.com>

--- assets/js/display/internal.js
+++ assets/js/display/internal.js
@@ -1322,7 +1322,7 @@ function processAnnouncements() {
                                     page.attr("width", `${(($('#scaled-content').height() / maxHeight) * 70)}%`);
                                     var scaleX = 1, scaleY = 1;
                                     scaleX = (maxWidth / $('#scaled-content').width()) * 0.95;
-                                    scaleY = (maxHeight / $('#scaled-content').height()) * 0.70;
+                                    scaleY = (maxHeight / $('#scaled-content').height()) * 0.80;
                                     basePage.scaleX = scaleX;
                                     basePage.scaleY = scaleY;
                                     basePage.scale = (scaleX > scaleY) ? scaleY : scaleX;
@@ -1385,7 +1385,7 @@ function processAnnouncements() {
                                     page.attr("width", `${(($('#scaled-content').height() / maxHeight) * 70)}%`);
                                     var scaleX = 1, scaleY = 1;
                                     scaleX = (maxWidth / $('#scaled-content').width()) * 0.95;
-                                    scaleY = (maxHeight / $('#scaled-content').height()) * 0.70;
+                                    scaleY = (maxHeight / $('#scaled-content').height()) * 0.80;
                                     basePage.scaleX = scaleX;
                                     basePage.scaleY = scaleY;
                                     basePage.scale = (scaleX > scaleY) ? scaleY : scaleX;
