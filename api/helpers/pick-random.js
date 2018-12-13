b9d85f64feadf3043e4c3cb61ba09ef2b077c392
Patrick Schmalstig <xanaftp@gmail.com>
2018-12-12 19:53:06 -0500

    Added: sails.helpers.pickRandom
    Signed-off-by: Patrick Schmalstig <xanaftp@gmail.com>

---
+++ api/helpers/pick-random.js
@@ -0,0 +1,50 @@
+/* global sails */
+
+module.exports = {
+
+
+  friendlyName: 'sails.helpers.pickRandom',
+
+
+  description: 'Pick a random item from an array. Returns an object {item: itempicked, newArray: [array]}.',
+
+
+  inputs: {
+      items: {
+          type: 'ref',
+          required: true,
+          description: 'The array to pick something random from.'
+      },
+      remove: {
+          type: 'boolean',
+          defaultsTo: false,
+          description: 'If true, the chosen item from the array will be deleted from the return of newArray.'
+      }
+  },
+
+
+  fn: async function (inputs, exits) {
+      sails.log.debug('Helper pickRandom called.');
+    try {
+        if (typeof inputs.items.length === 'undefined' || inputs.items.length <= 0)
+            return exits.success({item: null, newArray: []});
+        
+        // Choose a random item
+        var random = Math.floor(Math.random() * (inputs.items.length || 0));
+        var item = inputs.items[random];
+        
+        // Remove the item from the array if necessary
+        if (inputs.remove)
+            delete inputs.items[random];
+        
+        return exits.success({item: item, newArray: inputs.items});
+        
+        
+    } catch (e) {
+        return exits.error(e);
+    }
+  }
+
+
+};
+
