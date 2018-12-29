9a511f3b679df9a327b6d14c64294ddd177a8e6c
Patrick Schmalstig <xanaftp@gmail.com>
2018-12-29 12:55:37 -0500

    tweak
    Signed-off-by: Patrick Schmalstig <xanaftp@gmail.com>

--- api/helpers/rest/change-radio-dj.js
+++ api/helpers/rest/change-radio-dj.js
@@ -46,6 +46,8 @@ module.exports = {
                                 // Throw an error so that error.post does not get called, which is sometimes called after this helper finishes.
                                 throw new Error(`There are no healthy RadioDJ instances to switch to at this time.`);
                             }
+                            
+                            // Patrick Schmalstig cannot spell worth a shit
                             return true;
                         });
                 await Promise.all(maps);
