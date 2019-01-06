e4d48fa8c77dc86e4f80ebbc5fe3058966b7da68
Patrick Schmalstig <xanaftp@gmail.com>
2019-01-06 02:39:07 -0500

    more stuff
    Signed-off-by: Patrick Schmalstig <xanaftp@gmail.com>

--- api/controllers/test/showtime.js
+++ api/controllers/test/showtime.js
@@ -13,58 +13,59 @@ module.exports = {
     fn: async function (inputs, exits) {
 
         var DJs = {};
-        
+
         var records = await Attendance.find({createdAt: {">=": "2019-01-06 02:00:00"}});
         var maps = records.map(async (record) => {
-            await Logs.update({createdAt: {">=": moment(record.actualStart).toISOString(true), "<": moment(record.actualEnd).toISOString(true)}}, {attendanceID: record.ID});
+            await Logs.update({and: [{createdAt: {">=": moment(record.actualStart).toISOString(true)}}, {createdAt: {"<": moment(record.actualEnd).toISOString(true)}}]}, {attendanceID: record.ID});
         });
-        
-        /*
-        DJs[0] = {name: "EVERYONE", showtime: 0, listeners: 0};
-        var records = await Djs.find();
-        records.map(record => DJs[record.ID] = {name: record.name, showtime: 0, listeners: 0});
+        await Promise.all(maps);
 
-        var curDJ = null;
-        var startTime = moment("2018-10-01 00:00:00");
-        var prevTime = moment("2018-10-01 00:00:00");
-        var tShowTime = 0;
-        var tListenerMinutes = 0;
-        var prevListeners = 0;
-        var records = await Listeners.find({createdAt: {"<=": "2018-10-01 00:00:00"}}).sort("createdAt ASC");
-        records
-                .map(record => {
-                    if (record.dj !== curDJ)
-                    {
-                        if (curDJ !== null)
-                        {
-                            if (typeof DJs[curDJ] !== 'undefined')
-                            {
-                                tShowTime += (moment(record.createdAt).diff(moment(prevTime), 'seconds') / 60);
-                                tListenerMinutes += (moment(record.createdAt).diff(moment(prevTime), 'seconds') / 60) * prevListeners;
-                                DJs[curDJ].showtime += (moment(record.createdAt).diff(moment(prevTime), 'seconds') / 60);
-                                DJs[curDJ].listeners += (moment(record.createdAt).diff(moment(prevTime), 'seconds') / 60) * prevListeners;
-                                DJs[0].showtime += (moment(record.createdAt).diff(moment(prevTime), 'seconds') / 60);
-                                DJs[0].listeners += (moment(record.createdAt).diff(moment(prevTime), 'seconds') / 60) * prevListeners;
-                            }
-                        }
-                        startTime = moment(record.createdAt);
-                        tShowTime = 0;
-                        tListenerMinutes = 0;
-                    } else if (curDJ !== null) {
-                        if (typeof DJs[record.dj] === 'undefined')
-                            return;
-                        tShowTime += (moment(record.createdAt).diff(moment(prevTime), 'seconds') / 60);
-                        tListenerMinutes += (moment(record.createdAt).diff(moment(prevTime), 'seconds') / 60) * prevListeners;
-                        DJs[record.dj].showtime += (moment(record.createdAt).diff(moment(prevTime), 'seconds') / 60);
-                        DJs[record.dj].listeners += (moment(record.createdAt).diff(moment(prevTime), 'seconds') / 60) * prevListeners;
-                        DJs[0].showtime += (moment(record.createdAt).diff(moment(prevTime), 'seconds') / 60);
-                        DJs[0].listeners += (moment(record.createdAt).diff(moment(prevTime), 'seconds') / 60) * prevListeners;
-                    }
-                    curDJ = record.dj;
-                    prevTime = moment(record.createdAt);
-                    prevListeners = record.listeners;
-                });
-                */
+        /*
+         DJs[0] = {name: "EVERYONE", showtime: 0, listeners: 0};
+         var records = await Djs.find();
+         records.map(record => DJs[record.ID] = {name: record.name, showtime: 0, listeners: 0});
+         
+         var curDJ = null;
+         var startTime = moment("2018-10-01 00:00:00");
+         var prevTime = moment("2018-10-01 00:00:00");
+         var tShowTime = 0;
+         var tListenerMinutes = 0;
+         var prevListeners = 0;
+         var records = await Listeners.find({createdAt: {"<=": "2018-10-01 00:00:00"}}).sort("createdAt ASC");
+         records
+         .map(record => {
+         if (record.dj !== curDJ)
+         {
+         if (curDJ !== null)
+         {
+         if (typeof DJs[curDJ] !== 'undefined')
+         {
+         tShowTime += (moment(record.createdAt).diff(moment(prevTime), 'seconds') / 60);
+         tListenerMinutes += (moment(record.createdAt).diff(moment(prevTime), 'seconds') / 60) * prevListeners;
+         DJs[curDJ].showtime += (moment(record.createdAt).diff(moment(prevTime), 'seconds') / 60);
+         DJs[curDJ].listeners += (moment(record.createdAt).diff(moment(prevTime), 'seconds') / 60) * prevListeners;
+         DJs[0].showtime += (moment(record.createdAt).diff(moment(prevTime), 'seconds') / 60);
+         DJs[0].listeners += (moment(record.createdAt).diff(moment(prevTime), 'seconds') / 60) * prevListeners;
+         }
+         }
+         startTime = moment(record.createdAt);
+         tShowTime = 0;
+         tListenerMinutes = 0;
+         } else if (curDJ !== null) {
+         if (typeof DJs[record.dj] === 'undefined')
+         return;
+         tShowTime += (moment(record.createdAt).diff(moment(prevTime), 'seconds') / 60);
+         tListenerMinutes += (moment(record.createdAt).diff(moment(prevTime), 'seconds') / 60) * prevListeners;
+         DJs[record.dj].showtime += (moment(record.createdAt).diff(moment(prevTime), 'seconds') / 60);
+         DJs[record.dj].listeners += (moment(record.createdAt).diff(moment(prevTime), 'seconds') / 60) * prevListeners;
+         DJs[0].showtime += (moment(record.createdAt).diff(moment(prevTime), 'seconds') / 60);
+         DJs[0].listeners += (moment(record.createdAt).diff(moment(prevTime), 'seconds') / 60) * prevListeners;
+         }
+         curDJ = record.dj;
+         prevTime = moment(record.createdAt);
+         prevListeners = record.listeners;
+         });
+         */
 
         /*
          var records = await Attendance.find();
