729730585b673d749759b993a3e9983e7bc11d3b
Patrick Schmalstig <xanaftp@gmail.com>
2019-01-24 09:12:40 -0500

    use UTC for strike counter
    Signed-off-by: Patrick Schmalstig <xanaftp@gmail.com>

--- assets/js/strike/strike.js
+++ assets/js/strike/strike.js
@@ -4,12 +4,12 @@ var theTime = null;
 $.ajax('/meta/get', {})
         .then(
                 function success(resHTML) {
-                    theTime = moment(resHTML.time);
+                    theTime = moment.utc(resHTML.time);
                     setInterval(function () {
                         theTime = moment(theTime).add(1, 'seconds');
                         var temp = document.querySelector(`#aaup-strike`);
                         if (temp)
-                            temp.innerHTML = moment.duration(moment(theTime).diff(moment("2019-01-22 08:00:00"))).format("d [days], h:mm:ss");
+                            temp.innerHTML = moment.duration(moment.utc(theTime).diff(moment.utc("2019-01-22 08:00:00"))).format("d [days], h:mm:ss");
                     }, 1000);
                 },
                 function fail(data, status) {
