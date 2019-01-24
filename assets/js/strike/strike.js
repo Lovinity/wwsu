3e9456763e564cd951c8bbd8c85b32dfb8d66df9
Patrick Schmalstig <xanaftp@gmail.com>
2019-01-24 09:16:36 -0500

    whoops
    Signed-off-by: Patrick Schmalstig <xanaftp@gmail.com>

--- assets/js/strike/strike.js
+++ assets/js/strike/strike.js
@@ -9,7 +9,7 @@ $.ajax('/meta/get', {})
                         theTime = moment(theTime).add(1, 'seconds');
                         var temp = document.querySelector(`#aaup-strike`);
                         if (temp)
-                            temp.innerHTML = moment.duration(moment.utc(theTime).diff(moment.utc("2019-01-22 08:00:00"))).format("d [days], h:mm:ss");
+                            temp.innerHTML = moment.duration(moment.utc(theTime).diff(moment.utc("2019-01-22 13:00:00"))).format("d [days], h:mm:ss");
                     }, 1000);
                 },
                 function fail(data, status) {
