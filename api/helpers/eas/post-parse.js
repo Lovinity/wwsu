c60dab7bd3b7a4f0ce3504b6432373a62a42af0d
Patrick Schmalstig <xanaftp@gmail.com>
2019-02-24 10:17:22 -0500

    post parse EAS changes with how NWS alerts are removed
    Signed-off-by: Patrick Schmalstig <xanaftp@gmail.com>

--- api/helpers/eas/post-parse.js
+++ api/helpers/eas/post-parse.js
@@ -47,22 +47,19 @@ module.exports = {
             sails.log.silly(records);
 
             var maps = records.map(async record => {
-                // Remove NWS alerts no longer listed in the CAPS; we assume those alerts were expired.
-                // Temporarily disabled until a bug can be fixed.
-                /*
-                if (record.source === 'NWS' && Eas.activeCAPS.indexOf(record.reference) === -1)
+
+                // Trigger a non-fetched update of all NWS records still active so that the updatedAt is updated, and therefore the record isn't removed.
+                if (record.source === 'NWS' && Eas.activeCAPS.indexOf(record.reference) !== -1)
                 {
-                    sails.log.verbose(`Record ${record.ID} is to be deleted. It no longer exists in NWS CAPS.`);
-                    await Eas.destroy({ID: record.ID}).fetch()
+                    await Eas.update({ID: record.ID}, {updatedAt: moment().toISOString(true)})
                             .tolerate((err) => {
                                 sails.log.error(err);
                             });
                     return true;
                 }
-                */
-
-                // Remove expired alerts
-                if (moment().isAfter(moment(record.expires)))
+                
+                // Remove expired alerts and NWS records that have not been updated (by the above) in 5 or more minutes.
+                if ((record.source === 'NWS' && moment().isAfter(moment(record.updatedAt).add(5, 'minutes'))) || moment().isAfter(moment(record.expires)))
                 {
                     sails.log.verbose(`Record ${record.ID} is to be deleted. It has expired.`);
                     await Eas.destroy({ID: record.ID}).fetch()
