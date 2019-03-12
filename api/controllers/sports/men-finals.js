f65677a8aca5e16a6aeed5bee6701238b9668142
Patrick Schmalstig <xanaftp@gmail.com>
2019-03-12 15:32:55 -0400

    score update

--- api/controllers/sports/men-finals.js
+++ api/controllers/sports/men-finals.js
@@ -3,7 +3,7 @@
 module.exports = async function public(req, res) {
     sails.log.debug('Controller called.');
     
-    return res.view('sports/mensemifinals', {layout: 'sports/layout', timestamp: moment().valueOf()});
+    return res.view('sports/menfinals', {layout: 'sports/layout', timestamp: moment().valueOf()});
 
 };
 
