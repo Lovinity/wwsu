d493798940a1433b3be76c0631ebde7d66bb6da7
Patrick Schmalstig <xanaftp@gmail.com>
2019-03-18 14:54:13 -0400

    start of helper onesignal.sendEvent
    Signed-off-by: Patrick Schmalstig <xanaftp@gmail.com>

---
+++ api/helpers/onesignal/send-event.js
@@ -0,0 +1,30 @@
+module.exports = {
+
+
+  friendlyName: 'Send event',
+
+
+  description: '',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+    success: {
+      description: 'All done.',
+    },
+
+  },
+
+
+  fn: async function (inputs) {
+    // TODO
+  }
+
+
+};
+
