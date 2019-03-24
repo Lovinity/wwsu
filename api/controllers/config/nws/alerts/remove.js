50e0f42ea34e4ca74e4e3429a9acbeb3f409e1e2
Patrick Schmalstig <xanaftp@gmail.com>
2019-03-24 16:46:02 -0400

    eas config

---
+++ api/controllers/config/nws/alerts/remove.js
@@ -0,0 +1,40 @@
+/* global sails */
+
+module.exports = {
+
+    friendlyName: 'config / nws / alerts / remove',
+
+    description: 'Do not trigger the internal EAS for the provided NWS alert anymore.',
+
+    inputs: {
+        alert: {
+            type: 'string',
+            required: true,
+            description: `The name of the alert as provided by the NWS to remove from configuration, case sensitive. Example: "Tornado Warning".`
+        },
+    },
+
+    exits: {
+
+    },
+
+    fn: async function (inputs, exits) {
+        sails.log.debug('Controller config/nws/alerts/remove called.');
+
+        try {
+            delete sails.config.custom.EAS.alerts[inputs.alert];
+
+            // broadcast changes over websockets
+            sails.sockets.broadcast('config', 'config', {update: {EAS: sails.config.custom.EAS}});
+
+            return exits.success();
+        } catch (e) {
+            return exits.error(e);
+        }
+
+    }
+
+
+};
+
+
---
+++ api/controllers/config/nws/caps/remove.js
@@ -0,0 +1,42 @@
+/* global sails */
+
+module.exports = {
+
+    friendlyName: 'config / nws / caps / remove',
+
+    description: 'Stop monitoring for severe weather alerts for the specified NWS county or zone code.',
+
+    inputs: {
+        code: {
+            type: 'string',
+            required: true,
+            description: `The county or zone code to remove from the EAS.`
+        }
+    },
+
+    exits: {
+
+    },
+
+    fn: async function (inputs, exits) {
+        sails.log.debug('Controller config/nws/caps/remove called.');
+
+        try {
+            sails.config.custom.EAS.NWSX
+                    .filter((caps) => caps.code === inputs.code)
+                    .map((caps, index) => delete sails.config.custom.EAS.NWSX[index]);
+
+            // broadcast changes over websockets
+            sails.sockets.broadcast('config', 'config', {update: {EAS: sails.config.custom.EAS}});
+
+            return exits.success();
+        } catch (e) {
+            return exits.error(e);
+        }
+
+    }
+
+
+};
+
+
