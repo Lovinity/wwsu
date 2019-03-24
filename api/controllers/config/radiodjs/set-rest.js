c1ec24a9959612bca3e643eedd3d9113e4efc6ef
Patrick Schmalstig <xanaftp@gmail.com>
2019-03-24 15:53:58 -0400

    xp config

---
+++ api/controllers/config/radiodjs/set-rest.js
@@ -0,0 +1,39 @@
+/* global sails, Status, Meta */
+
+module.exports = {
+
+    friendlyName: 'config / radiodjs / set-rest',
+
+    description: 'Change the password that every RadioDJ REST server is using.',
+
+    inputs: {
+        auth: {
+            type: 'string',
+            required: true,
+            description: `The new REST auth password that should be used for all configured RadioDJs.`
+        },
+    },
+
+    exits: {
+
+    },
+
+    fn: async function (inputs, exits) {
+        sails.log.debug('Controller config/radiodjs/set-rest called.');
+
+        try {
+            sails.config.custom.rest.auth = inputs.auth;
+
+            // Do not update rest auth password changes through websockets; it's a secret.
+
+            return exits.success();
+        } catch (e) {
+            return exits.error(e);
+        }
+
+    }
+
+
+};
+
+
