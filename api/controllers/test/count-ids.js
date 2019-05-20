e09351fb38a3562870ade772a3e2d2b98eedb3ba
Patrick Schmalstig <engineer@wwsu1069.org>
2019-05-20 11:55:22 -0400

    Potential counting conflict

--- api/controllers/test/count-ids.js
+++ api/controllers/test/count-ids.js
@@ -18,18 +18,21 @@ module.exports = {
     fn: async function (inputs, exits) {
 
         try {
-            var records3 = await Logs.find({logtype: "id"});
+            var records3 = await Logs.find({ logtype: "id" });
+            var records4 = {};
             records3
-            .filter((record) => record.attendanceID !== null)
-            .map((record) => {
-                (async (record2) => {
-                    var attendanceRecord = await Attendance.findOne({ID: record2.attendanceID});
-                    if (attendanceRecord)
-                    {
-                        await Attendance.update({ID: record2.attendanceID}, {missedIDs: attendanceRecord.missedIDs + 1}).fetch();
-                    }
-                })(record);
-            });
+                .filter((record) => record.attendanceID !== null)
+                .map((record) => {
+                    if (typeof records4[record.attendanceID] === `undefined`)
+                        records4[record.attendanceID] = 0;
+                    records4[record.attendanceID] += 1;
+                });
+
+            for (var record in records4) {
+                if (records4.hasOwnProperty(record)) {
+                    await Attendance.update({ ID: record }, { missedIDs: records4[record] }).fetch();
+                }
+            }
 
             return exits.success();
         } catch (e) {
