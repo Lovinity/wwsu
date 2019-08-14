5233635e43fda7ec7810808bd8340e3731e5c0c4
Patrick Schmalstig <engineer@wwsu1069.org>
2019-08-14 09:53:42 -0400

    Get rid of coffee as well

--- tasks/register/compileAssets.js
+++ tasks/register/compileAssets.js
@@ -13,6 +13,6 @@ module.exports = function (grunt) {
     // 'jst:dev',
     'less:dev',
     'copy:dev',
-    'coffee:dev'
+    // 'coffee:dev'
   ])
 }
--- tasks/register/syncAssets.js
+++ tasks/register/syncAssets.js
@@ -12,6 +12,6 @@ module.exports = function (grunt) {
     // 'jst:dev',
     'less:dev',
     'sync:dev',
-    'coffee:dev'
+    // 'coffee:dev'
   ])
 }
