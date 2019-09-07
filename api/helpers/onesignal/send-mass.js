331a93f26f988fca60e1be9802f6a9c9fb9b0db1
Patrick Schmalstig <engineer@wwsu1069.org>
2019-09-06 23:38:09 -0400

    Forgot onesignal.sendMass helper

---
+++ api/helpers/onesignal/send-mass.js
@@ -0,0 +1,47 @@
+module.exports = {
+
+  friendlyName: 'sails.helpers.onesignal.sendMass',
+
+  description: 'Send push notifications out for director-related matters.',
+
+  inputs: {
+    type: {
+      type: 'string',
+      required: true,
+      description: `Send this to everyone subscribed to the provided subscription type.`
+    },
+
+    title: {
+      type: `string`,
+      required: true,
+      description: `Title of the notification`
+    },
+
+    content: {
+      type: 'string',
+      required: true,
+      description: `Additional information / content.`
+    }
+  },
+
+  fn: async function (inputs, exits) {
+    try {
+      var devices = []
+
+      // Find any recurring subscriptions to the event.
+      var records = await sails.models.subscribers.find({ type: inputs.type })
+      records.map((record) => devices.push(record.device))
+
+      // If we have at least 1 person to receive a push notification, continue
+      if (devices.length > 0) {
+        await sails.helpers.onesignal.send(devices, `message`, inputs.title, inputs.content, (60 * 60 * 24 * 7))
+      }
+
+      return exits.success(true)
+    } catch (e) {
+      // No erroring if there's an error; just ignore it
+      sails.log.error(e)
+      return exits.success(false)
+    }
+  }
+}
