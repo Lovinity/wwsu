7a5eee8b7dcba08060c2c8829c64edcf86f7f25f
Patrick Schmalstig <engineer@wwsu1069.org>
2019-09-09 11:57:57 -0400

    more analytics

--- api/controllers/analytics/yearly.js
+++ api/controllers/analytics/yearly.js
@@ -15,7 +15,7 @@ module.exports = {
       var start = moment().subtract(i, 'months')
       var end = moment().subtract(i - 1, 'months')
 
-      months[moment(start).format('MMMM YYYY')] = { streamHits: 0, tracksLiked: 0, webMessages: 0, requestsPlaced: 0 }
+      months[moment(start).format('MMMM YYYY')] = { streamHits: 0, listenerHoursTotal: 0, listenerHoursShows: 0, listenerToShowtimeRatioTotal: 0, listenerToShowtimeRatioShows: 0, tracksLiked: 0, webMessages: 0, requestsPlaced: 0 }
 
       var records = await sails.models.listeners.find({ createdAt: { '>=': moment(start).toISOString(true), '<': moment(end).toISOString(true) } }).sort(`createdAt ASC`)
       var prevAmount = 0
@@ -25,7 +25,12 @@ module.exports = {
         }
         prevAmount = record.listeners
       })
-
+      months[moment(start).format('MMMM YYYY')].listenerHoursTotal = await sails.models.attendance.sum('listenerMinutes', { createdAt: { '>=': moment(start).toISOString(true), '<': moment(end).toISOString(true) } }) / 60
+      var showTime = await sails.models.attendance.sum('showTime', { createdAt: { '>=': moment(start).toISOString(true), '<': moment(end).toISOString(true) } }) / 60
+      months[moment(start).format('MMMM YYYY')].listenerToShowtimeRatioTotal = (showTime > 0) ? months[moment(start).format('MMMM YYYY')].listenerHoursTotal / showTime : 0
+      months[moment(start).format('MMMM YYYY')].listenerHoursShows = await sails.models.attendance.sum('listenerMinutes', { dj: { '!=': null }, createdAt: { '>=': moment(start).toISOString(true), '<': moment(end).toISOString(true) } }) / 60
+      var showTimeB = await sails.models.attendance.sum('showTime', { dj: { '!=': null }, createdAt: { '>=': moment(start).toISOString(true), '<': moment(end).toISOString(true) } }) / 60
+      months[moment(start).format('MMMM YYYY')].listenerToShowtimeRatioShows = (showTime > 0) ? months[moment(start).format('MMMM YYYY')].listenerHoursShows / showTimeB : 0
       months[moment(start).format('MMMM YYYY')].tracksLiked = await sails.models.songsliked.count({ createdAt: { '>=': moment(start).toISOString(true), '<': moment(end).toISOString(true) } })
       months[moment(start).format('MMMM YYYY')].webMessages = await sails.models.messages.count({ or: [{ from: { startsWith: 'website-' } }, { to: ['DJ', 'DJ-private'] }], createdAt: { '>=': moment(start).toISOString(true), '<': moment(end).toISOString(true) } })
       months[moment(start).format('MMMM YYYY')].requestsPlaced = await sails.models.requests.count({ createdAt: { '>=': moment(start).toISOString(true), '<': moment(end).toISOString(true) } })
