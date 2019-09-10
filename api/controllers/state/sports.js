668bbdbaf761b82b27e411dabd72c0200d4a1893
Patrick Schmalstig <engineer@wwsu1069.org>
2019-09-10 19:48:43 -0400

    wooooow...

--- api/controllers/state/sports.js
+++ api/controllers/state/sports.js
@@ -86,10 +86,10 @@ module.exports = {
         // await sails.helpers.error.count('sportsQueue');
 
         // Change meta
-        sails.helpers.meta.change.with({ queueFinish: moment().add(queueLength, 'seconds').toISOString(true), state: 'automation_sports', show: inputs.sport, topic: inputs.topic, trackStamp: null, lastID: moment().toISOString(true), webchat: inputs.webchat })
+        await sails.helpers.meta.change.with({ queueFinish: moment().add(queueLength, 'seconds').toISOString(true), state: 'automation_sports', show: inputs.sport, topic: inputs.topic, trackStamp: null, lastID: moment().toISOString(true), webchat: inputs.webchat })
       } else {
         // Otherwise, just update metadata but do not do anything else
-        sails.helpers.meta.change.with({ show: inputs.sport, topic: inputs.topic, trackStamp: null, webchat: inputs.webchat })
+        await sails.helpers.meta.change.with({ show: inputs.sport, topic: inputs.topic, trackStamp: null, webchat: inputs.webchat })
       }
 
       await sails.helpers.error.reset('automationBreak')
