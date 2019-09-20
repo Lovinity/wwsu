303756ddd64fe491c081dc3d0e87a80f87f457e2
Patrick Schmalstig <engineer@wwsu1069.org>
2019-09-20 15:11:58 -0400

    ...

--- api/controllers/dj.js
+++ api/controllers/dj.js
@@ -1,4 +1,4 @@
 module.exports = async function listen (req, res) {
   sails.log.debug('Controller listen called.')
-  return res.view('dj/home', { layout: 'dj/layout', timestamp: moment().format('YYYYMMDD') })
+  return res.view('dj/panel', { layout: 'dj/layout', timestamp: moment().format('YYYYMMDD') })
 }
