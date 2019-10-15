503818cafa851955f2130c4831add85b256044cc
Patrick Schmalstig <engineer@wwsu1069.org>
2019-10-15 13:44:38 -0400

    Bug fix attempt

--- api/helpers/rest/cmd.js
+++ api/helpers/rest/cmd.js
@@ -30,27 +30,24 @@ module.exports = {
     // arg supplied? Load it in memory.
     if (typeof inputs.arg !== 'undefined' && inputs.arg !== null) { endstring = '&arg=' + inputs.arg }
 
-    // If timeout is 0, resolve the promise but continue execution (so do not return it).
-    if (inputs.timeout === 0) {
-      exits.success()
-      inputs.timeout = 10000
-    }
-
     try {
       // Query REST
       // LINT: do NOT camel case; these are needle parameters.
       // eslint-disable-next-line camelcase
-      needle('get', sails.models.meta.memory.radiodj + '/opt?auth=' + sails.config.custom.rest.auth + '&command=' + inputs.command + endstring, {}, { open_timeout: inputs.timeout, response_timeout: inputs.timeout, read_timeout: inputs.timeout, headers: { 'Content-Type': 'application/json' } })
+      needle('get', sails.models.meta.memory.radiodj + '/opt?auth=' + sails.config.custom.rest.auth + '&command=' + inputs.command + endstring, {}, { open_timeout: inputs.timeout > 0 ? inputs.timeout : 10000, response_timeout: inputs.timeout > 0 ? inputs.timeout : 10000, read_timeout: inputs.timeout > 0 ? inputs.timeout : 10000, headers: { 'Content-Type': 'application/json' } })
         .then(async () => {
-          try {
+          if (inputs.timeout > 0) {
             return exits.success(true)
-          } catch (unusedE) {
-            return exits.success(false)
           }
         })
         .catch(async () => {
-          return exits.success(false)
+          if (inputs.timeout > 0) {
+            return exits.success(false)
+          }
         })
+      if (inputs.timeout === 0) {
+        return exits.success(false)
+      }
     } catch (unusedE2) {
       return exits.success(false)
     }
