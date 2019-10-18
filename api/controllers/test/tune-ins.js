b38d0761c7be6522169dbe3846e55d7af4ed006d
Patrick Schmalstig <engineer@wwsu1069.org>
2019-10-18 12:21:19 -0400

    try this instead

--- api/controllers/test/tune-ins.js
+++ api/controllers/test/tune-ins.js
@@ -12,14 +12,15 @@ module.exports = {
   },
 
   fn: async function (inputs, exits) {
-    var records = await sails.models.attendance.find({ showTime: { '!=': null } })
+    var records = await sails.models.attendance.find({ showTime: { '!=': null }, actualStart: { '!=': null }, actualEnd: { '!=': null } })
 
     if (records.length > 0) {
       var maps = records.map(async (record) => {
 
         var connections = 0
-        var prevListeners = 0
-        var records2 = await sails.helpers.analytics.listeners(record.actualStart, record.actualEnd)
+        var records2 = await sails.models.listeners.find({ createdAt: { '>=': currentRecord.actualStart }, createdAt: { '<': currentRecord.actualEnd } }).sort('createdAt ASC')
+        var prevListeners = await sails.models.listeners.find({ createdAt: { '<=': currentRecord.actualStart } }).sort('createdAt DESC').limit(1) || 0
+        if (prevListeners[ 0 ]) { prevListeners = prevListeners[ 0 ].listeners || 0 }
 
         if (records2.length > 0) {
           records2.map((record2) => {
