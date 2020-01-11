9d4db35d15630997f5ac458b6feb86eaf46388e8
Patrick Schmalstig <engineer@wwsu1069.org>
2020-01-10 20:37:47 -0500

    Tweaks

--- api/models/Calendar.js
+++ api/models/Calendar.js
@@ -1,110 +1,168 @@
 /**
- * Calendar.js
+ * calendar.js
  *
- * @description :: Container containing Google Calendar events.
+ * @description :: Calendar events.
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
  */
 
 module.exports = {
-  datastore: 'nodebase',
-  attributes: {
+    datastore: 'nodebase',
+    tableName: 'calendar7', // Temporary; remove when working and other calendar table was deleted.
+    attributes: {
 
-    ID: {
-      type: 'number',
-      autoIncrement: true
+        ID: {
+            type: 'number',
+            autoIncrement: true
+        },
+
+        type: {
+            type: 'string',
+            isIn: [ 'show', 'sports', 'remote', 'prerecord', 'genre', 'playlist', 'event', 'onair-booking', 'prod-booking', 'office-hours' ],
+            defaultsTo: 'event'
+        },
+
+        active: {
+            type: 'boolean',
+            defaultsTo: true,
+        },
+
+        priority: {
+            type: 'number',
+            allowNull: true,
+        },
+
+        hostDJ: {
+            type: 'number',
+            allowNull: true,
+        },
+
+        cohostDJ1: {
+            type: 'number',
+            allowNull: true
+        },
+
+        cohostDJ2: {
+            type: 'number',
+            allowNull: true
+        },
+
+        cohostDJ3: {
+            type: 'number',
+            allowNull: true
+        },
+
+        eventID: {
+            type: 'number',
+            allowNull: true
+        },
+
+        playlistID: {
+            type: 'number',
+            allowNull: true
+        },
+
+        director: {
+            type: 'number',
+            allowNull: true
+        },
+
+        hosts: {
+            type: 'string',
+            allowNull: true
+        },
+
+        name: {
+            type: 'string',
+            allowNull: true
+        },
+
+        description: {
+            type: 'string',
+            allowNull: true
+        },
+
+        logo: {
+            type: 'string',
+            allowNull: true
+        },
+
+        banner: {
+            type: 'string',
+            allowNull: true
+        },
+
+        start: {
+            type: 'ref',
+            columnType: 'date',
+            allowNull: true
+        },
+
+        end: {
+            type: 'ref',
+            columnType: 'date',
+            allowNull: true
+        },
+
+        duration: {
+            type: 'number',
+            min: 0,
+            defaultsTo: 60
+        },
+
+        schedule: {
+            type: 'json',
+            allowNull: true
+        },
+
     },
 
-    unique: {
-      type: 'string'
+    calendardb: undefined,
+
+    // Websockets standards
+    afterCreate: function (newlyCreatedRecord, proceed) {
+        var data = { insert: newlyCreatedRecord }
+        sails.models.calendar.calendardb.query('calendar', data);
+        sails.log.silly(`calendar socket: ${data}`)
+        sails.sockets.broadcast('calendar', 'calendar', data)
+        return proceed()
     },
 
-    active: {
-      type: 'number',
-      min: -1,
-      max: 2,
-      defaultsTo: 1
+    afterUpdate: function (updatedRecord, proceed) {
+        var data = { update: updatedRecord }
+        sails.models.calendar.calendardb.query('calendar', data);
+        sails.log.silly(`calendar socket: ${data}`)
+        sails.sockets.broadcast('calendar', 'calendar', data)
+
+        // If setting active to false, delete all exceptions and notify subscribers of a discontinued show
+        if (!updatedRecord.active) {
+            (async () => {
+                await sails.helpers.calendarexceptions.destroy({ calendarID: updatedRecord.ID }).fetch();
+                var event = sails.models.calendar.calendardb.processRecord(updatedRecord, {}, inputs.start);
+                await sails.helpers.onesignal.sendEvent(event, false, false)
+            })()
+        } else {
+            (async () => {
+                await sails.helpers.onesignal.sendEvent(event, false, true)
+            })()
+        }
+
+        return proceed()
     },
 
-    title: {
-      type: 'string',
-      defaultsTo: 'Unnamed Event'
-    },
+    afterDestroy: function (destroyedRecord, proceed) {
+        var data = { remove: destroyedRecord.ID }
+        sails.models.calendar.calendardb.query('calendar', data);
+        sails.log.silly(`calendar socket: ${data}`)
+        sails.sockets.broadcast('calendar', 'calendar', data)
 
-    description: {
-      type: 'string',
-      defaultsTo: ''
-    },
-
-    isDescriptionCustom: {
-      type: 'boolean',
-      defaultsTo: false
-    },
-
-    color: {
-      type: 'string',
-      defaultsTo: '#D50000'
-    },
-
-    allDay: {
-      type: 'boolean',
-      defaultsTo: false
-    },
-
-    start: {
-      type: 'ref',
-      columnType: 'datetime'
-    },
-
-    originalStart: {
-      type: 'ref',
-      columnType: 'datetime'
-    },
-
-    end: {
-      type: 'ref',
-      columnType: 'datetime'
-    },
-
-    originalEnd: {
-      type: 'ref',
-      columnType: 'datetime'
-    },
-
-    verify: {
-      type: 'string'
-    },
-
-    verifyMessage: {
-      type: 'string'
-    },
-
-    verifyTitleHTML: {
-      type: 'string'
+        // Remove all calendar exceptions
+        (async () => {
+            await sails.helpers.calendarexceptions.destroy({ calendarID: destroyedRecord.ID }).fetch();
+            var event = sails.models.calendar.calendardb.processRecord(destroyedRecord, {}, inputs.start);
+            await sails.helpers.onesignal.sendEvent(event, false, false)
+        })()
+        
+        return proceed()
     }
-  },
-
-  calendar: [],
-
-  // Websockets standards
-  afterCreate: function (newlyCreatedRecord, proceed) {
-    var data = { insert: newlyCreatedRecord }
-    sails.log.silly(`calendar socket: ${data}`)
-    sails.sockets.broadcast('calendar', 'calendar', data)
-    return proceed()
-  },
-
-  afterUpdate: function (updatedRecord, proceed) {
-    var data = { update: updatedRecord }
-    sails.log.silly(`calendar socket: ${data}`)
-    sails.sockets.broadcast('calendar', 'calendar', data)
-    return proceed()
-  },
-
-  afterDestroy: function (destroyedRecord, proceed) {
-    var data = { remove: destroyedRecord.ID }
-    sails.log.silly(`calendar socket: ${data}`)
-    sails.sockets.broadcast('calendar', 'calendar', data)
-    return proceed()
-  }
 
 }
