033a468ed145ae3b71a82d2523e08862ca3b3431
Patrick Schmalstig <xanaftp@gmail.com>
2020-04-29 22:12:58 -0400

    Debugging

--- config/sockets.js
+++ config/sockets.js
@@ -69,32 +69,32 @@ module.exports.sockets = {
         var references = record.ID
         if (record.active === 1) {
           if (record.action === 'permaban' || record.action === 'dayban' || record.action === 'showban') {
-            return proceed(new Error(`Your interactions with WWSU have been placed under review. Please email engineer@wwsu1069.org for further assistance. Please include the following reference number(s) in your email: ${references}`), false)
+            return proceed(`Your interactions with WWSU have been placed under review. Please contact WWSU. Please include the following reference number(s) in your email: ${references}`, false)
           }
         }
       }
     } catch (e) {
       sails.log.error(e)
-      return proceed(new Error(`An internal server error occurred when attempting to authorize this websocket connection.`), false)
+      return proceed(`An internal server error occurred when attempting to authorize this websocket connection.`, false)
     }
 
     // Allow requests from origin baseUrl and certain IPs and domains, otherwise require an authorized host header
-    if (handshake.headers && handshake.headers.origin && (handshake.headers.origin.startsWith(sails.config.custom.baseUrl || `http://localhost:${sails.config.port}`) || handshake.headers.origin.startsWith(`http://130.108.128.116`) || handshake.headers.origin.startsWith(`https://wwsu.wolform.me`))) {
-      return proceed(undefined, true)
+    if (handshake.headers && handshake.headers.origin && (handshake.headers.origin.startsWith(sails.config.custom.baseUrl || `http://localhost:${sails.config.port}`) || handshake.headers.origin.startsWith(`http://130.108.128.116`))) {
+      return proceed(`Origin allow`, false)
     } else {
       if (typeof handshake._query === 'undefined' || typeof handshake._query.host === 'undefined') {
-        return proceed(new Error(`You must provide a host query parameter to authorize this websocket connection.`), false)
+        return proceed(`You must provide a host query parameter to authorize this websocket connection.`, false)
       }
 
-      record = await sails.models.hosts.findOrCreate({ host: handshake._query.host }, { host: handshake._query.host, friendlyname: handshake._query.host })
+      var record = await sails.models.hosts.findOrCreate({ host: handshake._query.host }, { host: handshake._query.host, friendlyname: handshake._query.host })
 
       if (!record.authorized) {
-        return proceed(new Error(`The provided host is not yet authorized to connect to WWSU. Please have an administrator authorize this host.`), false)
+        return proceed(`The provided host is not yet authorized to connect to WWSU. Please have an administrator authorize this host.`, false)
       }
     }
 
     // At this point, allow the connection
-    return proceed(undefined, true)
+    return proceed(`End of function`, false)
   },
 
   /***************************************************************************
