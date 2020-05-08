3fe410d260522757b1a81dbf84494287b1b2a486
Patrick Schmalstig <xanaftp@gmail.com>
2020-05-08 17:02:07 -0400

    Types array for announcements/get to subscribe to multiple types at once

--- api/controllers/announcements/get.js
+++ api/controllers/announcements/get.js
@@ -5,10 +5,14 @@ module.exports = {
   description: 'Get announcements for the specified type. Subscribe to receive websockets for announcements event.',
 
   inputs: {
+    // TODO: remove "type" property when migrated fully to types array
     type: {
       type: 'string',
-      required: true,
-      description: 'Return announcements of the specified type; ensure websockets only include announcements of this type. Use "all" to return all announcements, but do not subscribe to a websocket.'
+      description: 'Subscribe to only this announcement type; ensure websockets only include announcements of this type. Use "all" to return all announcements.'
+    },
+    types: {
+      type: 'json',
+      description: 'Subscribe to the array of announcement types; ensure websockets only include announcements of this type. Use "all" to return all announcements.'
     },
     ID: {
       type: 'number',
@@ -21,22 +25,45 @@ module.exports = {
     sails.log.debug('Controller announcements/get called.')
 
     try {
-      // Determine which announcements to return based on type or ID; do not subscribe to websockets for "all" type
-      var records
-      if (inputs.type !== 'all' && (!inputs.ID || inputs.ID === null)) {
-        records = await sails.models.announcements.find({ type: inputs.type })
-      } else if (!inputs.ID || inputs.ID === null) {
-        records = await sails.models.announcements.find()
-      } else {
-        records = await sails.models.announcements.findOne({ ID: inputs.ID })
+      // If no types nor IDs provided, exit immediately with an empty array.
+      if (!inputs.type && !inputs.types && !inputs.ID) {
+        return exits.success([]);
       }
 
-      sails.log.verbose(`${records.length} records retrieved.`)
+      // Determine which announcements to return based on type or ID.
+      var records = [];
+      if (inputs.ID) {
+        records = await sails.models.announcements.findOne({ ID: inputs.ID })
+      } else if (inputs.type.indexOf('all') !== -1) {
+        records = await sails.models.announcements.find()
+        // Subscribe to websockets
+        if (this.req.isSocket) {
+          sails.sockets.join(this.req, `announcements-all`)
+          sails.log.verbose(`Request was a socket. Joined announcements-all.`)
+        }
+      } else if (inputs.type) {
+        records.push(await sails.models.announcements.find({ type: inputs.type }))
 
-      // Subscribe to websockets
-      if (this.req.isSocket) {
-        sails.sockets.join(this.req, `announcements-${inputs.type}`)
-        sails.log.verbose(`Request was a socket. Joined announcements-${inputs.type}.`)
+        sails.log.verbose(`${records.length} records retrieved.`)
+
+        // Subscribe to websockets
+        if (this.req.isSocket) {
+          sails.sockets.join(this.req, `announcements-${inputs.type}`)
+          sails.log.verbose(`Request was a socket. Joined announcements-${inputs.type}.`)
+        }
+      } else {
+        var maps = inputs.types.map(async (type) => {
+          records.push(await sails.models.announcements.find({ type: type }))
+
+          sails.log.verbose(`${records.length} records retrieved.`)
+
+          // Subscribe to websockets
+          if (this.req.isSocket) {
+            sails.sockets.join(this.req, `announcements-${type}`)
+            sails.log.verbose(`Request was a socket. Joined announcements-${type}.`)
+          }
+        });
+        await Promise.all(maps);
       }
 
       // Return records
