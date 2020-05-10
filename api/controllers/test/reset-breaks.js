ad70f32fb842105d2249c0995faf5a71953b3113
Patrick Schmalstig <xanaftp@gmail.com>
2020-05-09 21:56:39 -0400

    Debug

--- api/controllers/test/reset-breaks.js
+++ api/controllers/test/reset-breaks.js
@@ -10,11 +10,16 @@ module.exports = {
 
     fn: async function (inputs, exits) {
         var records = await sails.models.attendance.find();
+        sails.log.debug(`reset-breaks: Got ${records.length} attendance records`);
         var maps = records.map(async (record) => {
             var breaks = await sails.models.logs.count({ attendanceID: record.ID, logtype: 'break' });
+            sails.log.debug(`reset-breaks: Attendance ID ${record.ID} had ${breaks} breaks.`);
             await sails.models.attendance.update({ ID: record.ID }, { breaks: breaks });
+            sails.log.debug(`reset-breaks: Attendance ID ${record.ID} updated.`);
         });
         await Promise.all(maps);
+
+        sails.log.debug(`reset-breaks finished.`);
 
         return exits.success();
     }
