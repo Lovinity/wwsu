ef0ade3bc2d82aa9752d282e39ea148f9e065f80
Patrick Schmalstig <xanaftp@gmail.com>
2020-05-09 21:59:49 -0400

    Decrease queries

--- api/controllers/test/reset-breaks.js
+++ api/controllers/test/reset-breaks.js
@@ -10,12 +10,15 @@ module.exports = {
 
     fn: async function (inputs, exits) {
         var records = await sails.models.attendance.find();
+        var breakLogs = await sails.models.logs.find({ logtype: 'break' });
         sails.log.debug(`reset-breaks: Got ${records.length} attendance records`);
         var maps = records.map(async (record) => {
-            var breaks = await sails.models.logs.count({ attendanceID: record.ID, logtype: 'break' });
+            var breaks = breakLogs.filter((bl) => bl.attendanceID === record.ID).length;
             sails.log.debug(`reset-breaks: Attendance ID ${record.ID} had ${breaks} breaks.`);
-            await sails.models.attendance.update({ ID: record.ID }, { breaks: breaks });
-            sails.log.debug(`reset-breaks: Attendance ID ${record.ID} updated.`);
+            if (breaks > 0) {
+                await sails.models.attendance.update({ ID: record.ID }, { breaks: breaks });
+                sails.log.debug(`reset-breaks: Attendance ID ${record.ID} updated.`);
+            }
         });
         await Promise.all(maps);
 
