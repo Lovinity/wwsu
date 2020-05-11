3d87252a6070e5eeee47f52356fb2f2f62d4c910
Patrick Schmalstig <xanaftp@gmail.com>
2020-05-11 14:57:11 -0400

    Wrong order

--- api/helpers/analytics/listeners.js
+++ api/helpers/analytics/listeners.js
@@ -39,17 +39,17 @@ module.exports = {
       // First, get the listeners within the given range
       var records1 = await sails.models.listeners.find({ createdAt: { '>=': start.toISOString(true), '<': end.toISOString(true) } }).sort('createdAt ASC')
 
+      // If the latest returned record still falls before the provided end time, add the earliest record after the provided end time to close out the data.
+      if (typeof records1[ records1.length - 1 ] === 'undefined' || moment(end).isAfter(moment(records1[ records1.length - 1 ].createdAt))) {
+        var records2 = await sails.models.listeners.find({ createdAt: { '>': end.toISOString(true) } }).sort('createdAt ASC').limit(1)
+        if (records2) { records = records2.concat(records) }
+      }
+
       if (records1) { records = records.concat(records1) }
 
       // If the earliest returned record still falls after the provided start time, add the latest record before the provided start time as a baseline.
       if (typeof records1[ 0 ] === 'undefined' || moment(start).isBefore(moment(records1[ 0 ].createdAt))) {
         var records2 = await sails.models.listeners.find({ createdAt: { '<': start.toISOString(true) } }).sort('createdAt DESC').limit(1)
-        if (records2) { records = records2.concat(records) }
-      }
-
-      // If the latest returned record still falls before the provided end time, add the earliest record after the provided end time to close out the data.
-      if (typeof records1[ records1.length - 1 ] === 'undefined' || moment(end).isAfter(moment(records1[ records1.length - 1 ].createdAt))) {
-        var records2 = await sails.models.listeners.find({ createdAt: { '>': end.toISOString(true) } }).sort('createdAt ASC').limit(1)
         if (records2) { records = records2.concat(records) }
       }
 
