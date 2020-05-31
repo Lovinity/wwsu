23b6eac257a073323ab949fef7f3a78348ac45f4
Patrick Schmalstig <xanaftp@gmail.com>
2020-05-31 14:16:00 -0400

    Bug; Separate DJ merging to another controller

--- api/controllers/djs/edit.js
+++ api/controllers/djs/edit.js
@@ -13,7 +13,6 @@ module.exports = {
     },
     name: {
       type: 'string',
-      allowNull: true,
       custom: function (value) { // Prevent use of space dash space, or "; ", in names as this will cause problems in the system
         var temp2 = value.split(' - ')
         if (temp2.length > 1) { return false }
@@ -48,85 +47,25 @@ module.exports = {
     try {
       // Determine what needs updating
       var criteria = {}
-      if (inputs.name !== null && typeof inputs.name !== 'undefined' && inputs.name !== '') {
+
+      // If a name is provided, ensure this name is not already taken.
+      if (inputs.name && inputs.name !== '') {
         criteria.name = inputs.name
 
-        if (inputs.realName)
-          criteria.fullName = inputs.realName;
-
-        if (inputs.email)
-          criteria.email = inputs.email;
-
-        // Merge other DJ entries with the same name with this one
-        var dj = await sails.models.djs.find({ name: inputs.name })
-        var djID = await sails.models.djs.findOne({ ID: inputs.ID })
-        if (dj && dj.length > 0) {
-          var maps = dj
-            .filter((record) => record.ID !== inputs.ID)
-            .map(async record => {
-              var temp;
-              var updatedRecords = {};
-
-              // Update all XP records
-              await sails.models.xp.update({ dj: record.ID }, { dj: inputs.ID }).fetch();
-
-              // Update all attendance records
-              await sails.models.attendance.update({ dj: record.ID }, { dj: inputs.ID }).fetch();
-              await sails.models.attendance.update({ cohostDJ1: record.ID }, { cohostDJ1: inputs.ID }).fetch();
-              await sails.models.attendance.update({ cohostDJ2: record.ID }, { cohostDJ2: inputs.ID }).fetch();
-              await sails.models.attendance.update({ cohostDJ3: record.ID }, { cohostDJ3: inputs.ID }).fetch();
-
-              // Update all calendar records
-              temp = await sails.models.calendar.update({ hostDJ: record.ID }, { hostDJ: inputs.ID }).fetch();
-              temp.map((tmp) => updatedRecords[ tmp.ID ] = tmp);
-              temp = await sails.models.calendar.update({ cohostDJ1: record.ID }, { cohostDJ1: inputs.ID }).fetch();
-              temp.map((tmp) => updatedRecords[ tmp.ID ] = tmp);
-              temp = await sails.models.calendar.update({ cohostDJ2: record.ID }, { cohostDJ2: inputs.ID }).fetch();
-              temp.map((tmp) => updatedRecords[ tmp.ID ] = tmp);
-              temp = await sails.models.calendar.update({ cohostDJ3: record.ID }, { cohostDJ3: inputs.ID }).fetch();
-              temp.map((tmp) => updatedRecords[ tmp.ID ] = tmp);
-
-              // Regenerate hosts
-              for (var cal in updatedRecords) {
-                if (Object.prototype.hasOwnProperty.call(updatedRecords, cal)) {
-                  await sails.models.calendar.update({ ID: cal }, { hosts: await sails.helpers.calendar.generateHosts(updatedrecords[ cal ]) }).fetch();
-                }
-              }
-
-              updatedRecords = {};
-
-              // Update all calendar schedules
-              temp = await sails.models.schedule.update({ hostDJ: record.ID }, { hostDJ: inputs.ID }).fetch();
-              temp.map((tmp) => updatedRecords[ tmp.ID ] = tmp);
-              temp = await sails.models.schedule.update({ cohostDJ1: record.ID }, { cohostDJ1: inputs.ID }).fetch();
-              temp.map((tmp) => updatedRecords[ tmp.ID ] = tmp);
-              temp = await sails.models.schedule.update({ cohostDJ2: record.ID }, { cohostDJ2: inputs.ID }).fetch();
-              temp.map((tmp) => updatedRecords[ tmp.ID ] = tmp);
-              temp = await sails.models.schedule.update({ cohostDJ3: record.ID }, { cohostDJ3: inputs.ID }).fetch();
-              temp.map((tmp) => updatedRecords[ tmp.ID ] = tmp);
-
-              // Regenerate hosts
-              for (var cal in updatedRecords) {
-                if (Object.prototype.hasOwnProperty.call(updatedRecords, cal)) {
-                  await sails.models.schedule.update({ ID: cal }, { hosts: await sails.helpers.calendar.generateHosts(updatedrecords[ cal ]) }).fetch();
-                }
-              }
-
-              // Update lockToDJ in hosts
-              await sails.models.hosts.update({ lockToDJ: record.ID }, { lockToDJ: inputs.ID }).fetch()
-
-              // Remove the original record
-              await sails.models.djs.destroy({ ID: record.ID }).fetch()
-
-              // Edit meta if necessary
-              if (sails.models.meta.memory.dj === record.ID) { sails.helpers.meta.change.with({ dj: inputs.ID }) }
-              if (sails.models.meta.memory.cohostDJ1 === record.ID) { sails.helpers.meta.change.with({ cohostDJ1: inputs.ID }) }
-              if (sails.models.meta.memory.cohostDJ2 === record.ID) { sails.helpers.meta.change.with({ cohostDJ2: inputs.ID }) }
-              if (sails.models.meta.memory.cohostDJ3 === record.ID) { sails.helpers.meta.change.with({ cohostDJ3: inputs.ID }) }
-            })
-          await Promise.all(maps)
+        var dj = await sails.models.djs.findOne({ID: inputs.ID});
+        if (dj && dj.name !== inputs.name) {
+          var dj2 = await sails.models.djs.find({name: inputs.name});
+          if (dj2.length > 0) {
+            return exits.error(new Error("A DJ with this name already exists"));
+          }
         }
       }
+
+      if (inputs.realName)
+        criteria.realName = inputs.realName;
+
+      if (inputs.email)
+        criteria.email = inputs.email;
 
       // Encrypt login
       if (inputs.login !== null && typeof inputs.login !== 'undefined') { criteria.login = bcrypt.hashSync(inputs.login, 10) }
