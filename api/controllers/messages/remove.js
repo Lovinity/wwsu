a7d363e92f5bfff31aac1fd407e80f95a98c5f37
Patrick Schmalstig <xanaftp@gmail.com>
2020-06-25 15:27:59 -0400

    Add lockToDJ protection for message removing

--- api/controllers/messages/remove.js
+++ api/controllers/messages/remove.js
@@ -15,6 +15,10 @@ module.exports = {
     sails.log.debug('Controller messages/remove called.')
 
     try {
+      // Prevent removing messages if host is lockToDJ and the specified lockToDJ is not on the air
+      if (sails.models.meta.memory.host && this.req.payload.ID !== sails.models.meta.memory.host && this.req.payload.lockToDJ !== null && this.req.payload.lockToDJ !== sails.models.meta.memory.dj && this.req.payload.lockToDJ !== sails.models.meta.memory.cohostDJ1 && this.req.payload.lockToDJ !== sails.models.meta.memory.cohostDJ2 && this.req.payload.lockToDJ !== sails.models.meta.memory.cohostDJ3)
+        throw 'forbidden';
+
       // Remove the message
       await sails.helpers.messages.remove(inputs.ID)
       return exits.success()
