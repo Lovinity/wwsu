c398a6661209d102e93872180ee27da5c0e213db
Patrick Schmalstig <engineer@wwsu1069.org>
2020-09-22 16:50:39 -0400

    Records might need deep cloned

--- api/helpers/analytics/showtime.js
+++ api/helpers/analytics/showtime.js
@@ -1270,7 +1270,7 @@ module.exports = {
                 }
               }
             }
-            unique[ record.unique ] = record;
+            unique[ record.unique ] = _.cloneDeep(record);
           }
 
           tasksLeft--;
@@ -1287,8 +1287,6 @@ module.exports = {
 
       await new Promise(async (resolve2) => {
         var process3_3 = (record) => {
-          console.log(`Processing showtime record`);
-          console.dir(record);
           if (record.actualStart !== null && record.actualEnd !== null && record.happened === 1) {
             var maxBreaks = Object.keys(sails.config.custom.breaks).length > 0 ? record.showTime / (60 / Object.keys(sails.config.custom.breaks).length) : 0;
             var breakPoints = Math.min(maxBreaks, record.breaks);
