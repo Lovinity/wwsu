c9654409f9d22a9bc15da4a80107f482b557687d
Patrick Schmalstig <xanaftp@gmail.com>
2020-10-01 10:46:02 -0400

    ...

--- api/controllers/inventory/check-out.js
+++ api/controllers/inventory/check-out.js
@@ -7,55 +7,55 @@ module.exports = {
     item: {
       type: "number",
       required: true,
-      description: "The ID of the item being checked out",
+      description: "The ID of the item being checked out"
     },
 
     name: {
       type: "string",
       required: true,
-      description: "The full name of the person checking the item out",
+      description: "The full name of the person checking the item out"
     },
 
     checkOutDate: {
       type: "string",
       required: true,
       description: "The date/time the item was checked out. Defaults to now.",
-      custom: function (value) {
+      custom: function(value) {
         return moment(value).isValid();
-      },
+      }
     },
 
     checkOutCondition: {
       type: "string",
       isIn: ["Excellent", "Very Good", "Good", "Fair", "Poor", "Broken"],
       description:
-        "The condition of the item when checked out. Defaults to the condition specified in items.",
+        "The condition of the item when checked out. Defaults to the condition specified in items."
     },
 
     checkOutQuantity: {
       type: "number",
       required: true,
       description:
-        "The number of items checked out. Must be less than or equal to the item's available quantity.",
+        "The number of items checked out. Must be less than or equal to the item's available quantity."
     },
 
     checkOutNotes: {
       type: "string",
-      description: "Any additional check-out notes.",
+      description: "Any additional check-out notes."
     },
 
     checkInDue: {
       type: "string",
       description: "When the item is expected to be checked back in.",
-      custom: function (value) {
+      custom: function(value) {
         return moment(value).isValid();
-      },
-    },
+      }
+    }
   },
 
-  fn: async function (inputs) {
+  fn: async function(inputs) {
     // Check for item
-    var record = await sails.models.items.findOne({ ID: inputs.ID });
+    var record = await sails.models.items.findOne({ ID: inputs.item });
     if (!record) return "ITEM_NOT_FOUND";
 
     // Check if the item may be checked out
@@ -64,7 +64,7 @@ module.exports = {
     // Check if the specified quantity is available for checking out
     if (
       inputs.checkOutQuantity >
-      (await sails.helpers.inventory.getAvailableQuantity(inputs.ID))
+      (await sails.helpers.inventory.getAvailableQuantity(inputs.item))
     ) {
       return "QUANTITY_NOT_AVAILABLE";
     }
@@ -78,11 +78,11 @@ module.exports = {
       checkOutNotes: inputs.checkOutNotes,
       checkInDue: inputs.checkInDue
         ? moment(inputs.checkInDue).toISOString(true)
-        : undefined,
+        : undefined
     };
 
     await sails.models.checkout.create(criteria).fetch();
 
     return;
-  },
+  }
 };
