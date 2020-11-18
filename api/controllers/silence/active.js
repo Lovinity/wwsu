19042db037e61eb05b25c86f4fd40a3bcc90f581
Patrick Schmalstig <xanaftp@gmail.com>
2020-11-17 23:21:42 -0500

    Silence detection tweaks

--- api/controllers/silence/active.js
+++ api/controllers/silence/active.js
@@ -6,18 +6,10 @@ module.exports = {
 
   inputs: {},
 
-  fn: async function (inputs, exits) {
+  fn: async function(inputs, exits) {
     sails.log.debug("Controller silence/active called.");
 
     try {
-      // Activate status issue
-      await sails.helpers.status.change.with({
-        name: `silence`,
-        status: 2,
-        label: `Silence`,
-        data: `Silence / very low audio detected! Please ensure audio is going out over the air. If so, check the audio settings on the DJ Controls responsible for silence detection.`,
-      });
-
       // Log it
       await sails.models.logs
         .create({
@@ -27,7 +19,7 @@ module.exports = {
           logsubtype: sails.models.meta.memory.show,
           logIcon: `fas fa-volume-off`,
           title: `Silence detection alarm triggered.`,
-          event: `Silence / very low audio was detected for several seconds. Please investigate the cause and ensure this does not happen again.`,
+          event: `Silence / very low audio was detected for several seconds. Please investigate the cause and ensure this does not happen again.`
         })
         .fetch()
         .tolerate(() => {});
@@ -47,7 +39,7 @@ module.exports = {
             logsubtype: sails.models.meta.memory.show,
             logIcon: `fas fa-forward`,
             title: `Track was skipped due to silence detection.`,
-            event: `Track: ${sails.models.meta.automation[0].ID} (${sails.models.meta.automation[0].Artist} - ${sails.models.meta.automation[0].Title})<br />Please check this track to ensure it does not have more consecutive silence / very low audio than what silence detection is set at.`,
+            event: `Track: ${sails.models.meta.automation[0].ID} (${sails.models.meta.automation[0].Artist} - ${sails.models.meta.automation[0].Title})<br />Please check this track to ensure it does not have more consecutive silence / very low audio than what silence detection is set at.`
           })
           .fetch()
           .tolerate(() => {});
@@ -56,10 +48,22 @@ module.exports = {
         if (typeof sails.models.meta.automation[1] !== "undefined") {
           await sails.helpers.rest.cmd("PlayPlaylistTrack", 0);
         }
+
+        // Activate status issue
+        await sails.helpers.status.change.with({
+          name: `silence`,
+          status: 2,
+          label: `Silence`,
+          data: `Silence / very low audio detected! Track skipped due to potential silence.
+          <ul>
+            <li>Check track ${sails.models.meta.automation[0].ID} (${sails.models.meta.automation[0].Artist} - ${sails.models.meta.automation[0].Title}) in RadioDJ by looking at its waveform; ensure it does not have excessive silence / low audio.</li>
+            <li>If audio is going over the air, check the Audio Settings on the DJ Controls responsible for silence detection.</li>
+          </ul>`
+        });
       }
 
       // If we are in automation, and prevSilence is less than 3 minutes ago, assume an audio issue and switch RadioDJs
-      if (
+      else if (
         sails.models.status.errorCheck.prevSilence &&
         moment().isBefore(
           moment(sails.models.status.errorCheck.prevSilence).add(3, "minutes")
@@ -67,7 +71,7 @@ module.exports = {
         sails.models.meta.memory.state.startsWith("automation_")
       ) {
         await sails.helpers.meta.change.with({
-          changingState: `Switching automation instances due to no audio`,
+          changingState: `Switching automation instances due to no audio`
         });
 
         // Log the problem
@@ -79,10 +83,10 @@ module.exports = {
             logsubtype: "",
             logIcon: `fas fa-volume-mute`,
             title: `System changed active RadioDJ due to multiple silence alarms within 3 minutes.`,
-            event: `Please check to make sure all RadioDJs are functioning correctly and audio is not muted or very quiet from all RadioDJ sources.`,
+            event: `Please check to make sure all RadioDJs are functioning correctly and audio is not muted or very quiet from all RadioDJ sources.`
           })
           .fetch()
-          .tolerate((err) => {
+          .tolerate(err => {
             sails.log.error(err);
           });
         await sails.helpers.onesignal.sendMass(
@@ -93,21 +97,32 @@ module.exports = {
           )} because the silence detection system triggered multiple times. Please check DJ Controls.`
         );
 
+        await sails.helpers.status.change.with({
+          name: `silence`,
+          status: 1,
+          label: `Silence`,
+          data: `Silence / very low audio detected multiple times! Switched RadioDJ instances.
+          <ul>
+            <li>Silence detection was also triggered less than 3 minutes ago. Check to ensure all RadioDJs are working and audio is being sent out correctly. System switched RadioDJs as a precaution.</li>
+            <li>If audio is going over the air, check the Audio Settings on the DJ Controls responsible for silence detection.</li>
+          </ul>`
+        });
+
         // Find a RadioDJ to switch to
         var maps = sails.config.custom.radiodjs
           .filter(
-            (instance) => instance.rest === sails.models.meta.memory.radiodj
+            instance => instance.rest === sails.models.meta.memory.radiodj
           )
-          .map(async (instance) => {
+          .map(async instance => {
             var status = await sails.models.status.findOne({
-              name: `radiodj-${instance.name}`,
+              name: `radiodj-${instance.name}`
             });
             if (status && status.status !== 1) {
               await sails.helpers.status.change.with({
                 name: `radiodj-${instance.name}`,
                 label: `RadioDJ ${instance.label}`,
                 status: 2,
-                data: `Silence detection triggered multiple times. This RadioDJ might not be outputting audio.`,
+                data: `Silence detection triggered multiple times. This RadioDJ might not be outputting audio.`
               });
             }
             return true;
@@ -128,7 +143,7 @@ module.exports = {
       }
 
       // If we are not in automation, and prvSilence is less than 2 minutes ago, assume irresponsible DJ and automatically end the show (but go into automation_break).
-      if (
+      else if (
         sails.models.status.errorCheck.prevSilence &&
         moment().isBefore(
           moment(sails.models.status.errorCheck.prevSilence).add(2, "minutes")
@@ -136,7 +151,7 @@ module.exports = {
         !sails.models.meta.memory.state.startsWith("automation_")
       ) {
         await sails.helpers.meta.change.with({
-          changingState: `Ending current broadcast due to no audio`,
+          changingState: `Ending current broadcast due to no audio`
         });
 
         // Log the problem
@@ -148,10 +163,10 @@ module.exports = {
             logsubtype: sails.models.meta.memory.show,
             logIcon: `fas fa-microphone-slash`,
             title: `Broadcast terminated: Silence detection triggered twice within 2 minutes.`,
-            event: `This is likely the responsibility of the DJ / host. System terminated the broadcast and went to automation to stop the silence. Please investigate and, if necessary, speak with the host(s) about ensuring proper volume levels and avoiding on-air silence.<br />Broadcast: ${sails.models.meta.memory.show}`,
+            event: `This is likely the responsibility of the DJ / host. System terminated the broadcast and went to automation to stop the silence. Please investigate and, if necessary, speak with the host(s) about ensuring proper volume levels and avoiding on-air silence.<br />Broadcast: ${sails.models.meta.memory.show}`
           })
           .fetch()
-          .tolerate((err) => {
+          .tolerate(err => {
             sails.log.error(err);
           });
         await sails.helpers.onesignal.sendMass(
@@ -164,7 +179,25 @@ module.exports = {
           )} because of a long period of silence or multiple silences in a short time. Please talk with the DJ about avoiding silence on the air.`
         );
 
+        await sails.helpers.status.change.with({
+          name: `silence`,
+          status: 1,
+          label: `Silence`,
+          data: `Silence / very low audio detected! Broadcast was terminated.
+          <ul>
+            <li>Silence detection was triggered more than once in the last 2 minutes. The broadcast was terminated as a result.</li>
+            <li>If audio is going over the air, check the Audio Settings on the DJ Controls responsible for silence detection.</li>
+          </ul>`
+        });
+
         await sails.helpers.state.automation(true);
+      } else {
+        await sails.helpers.status.change.with({
+          name: `silence`,
+          status: 2,
+          label: `Silence`,
+          data: `Silence / very low audio detected! Please ensure audio is going out over the air and audio levels are not too low.`
+        });
       }
 
       sails.models.status.errorCheck.prevSilence = moment();
@@ -172,5 +205,5 @@ module.exports = {
     } catch (e) {
       return exits.error(e);
     }
-  },
+  }
 };
