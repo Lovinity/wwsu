25e9d6e9abd3509367792403e2bc768a104c1c31
Patrick Schmalstig <xanaftp@gmail.com>
2020-12-05 15:55:55 -0500

    Use only last 8 chars in new hosts for friendly name

--- api/controllers/auth/host.js
+++ api/controllers/auth/host.js
@@ -1,15 +1,14 @@
-var jwt = require('jsonwebtoken')
+var jwt = require("jsonwebtoken");
 
 module.exports = {
+  friendlyName: "Auth / Host",
 
-  friendlyName: 'Auth / Host',
-
-  description: 'Authorize a host and get a token.',
+  description: "Authorize a host and get a token.",
 
   inputs: {
     username: {
-      type: 'string',
-      description: 'The host to authorize.',
+      type: "string",
+      description: "The host to authorize.",
       required: true
     }
   },
@@ -26,23 +25,46 @@ module.exports = {
     }
   },
 
-  fn: async function (inputs, exits) {
-    sails.log.debug('Controller auth/host called.')
+  fn: async function(inputs, exits) {
+    sails.log.debug("Controller auth/host called.");
 
     try {
       // Verify the host first
-      var host = await sails.models.hosts.findOrCreate({ host: inputs.username }, { host: inputs.username, friendlyname: inputs.username })
+      var host = await sails.models.hosts.findOrCreate(
+        { host: inputs.username },
+        {
+          host: inputs.username,
+          friendlyname: `UNKNOWN ${inputs.username.substr(
+            inputs.username.length - 8
+          )}`
+        }
+      );
 
-      if (!host || !host.authorized) { return exits.noToken({ errToken: `The provided host either does not exist or is not authorized. To grant access, please use a DJ Controls with administrator privileges and authorize the host ${inputs.username}` }) }
+      if (!host || !host.authorized) {
+        return exits.noToken({
+          errToken: `The provided host either does not exist or is not authorized. To grant access, please use a DJ Controls with administrator privileges and authorize the host ${inputs.username}`
+        });
+      }
 
       // Generate the token valid for 10 minutes
-      var token = jwt.sign({ ID: host.ID, host: host.host, admin: host.admin, lockToDJ: host.lockToDJ, makeCalls: host.authorized && host.makeCalls, answerCalls: host.authorized && host.answerCalls, exp: Math.floor(Date.now() / 1000) + (60 * 10) }, sails.config.custom.secrets.host, { subject: 'host' })
+      var token = jwt.sign(
+        {
+          ID: host.ID,
+          host: host.host,
+          admin: host.admin,
+          lockToDJ: host.lockToDJ,
+          makeCalls: host.authorized && host.makeCalls,
+          answerCalls: host.authorized && host.answerCalls,
+          exp: Math.floor(Date.now() / 1000) + 60 * 10
+        },
+        sails.config.custom.secrets.host,
+        { subject: "host" }
+      );
 
       // Return the token as an object
-      return exits.success({ token: token, expires: (60000 * 10) })
+      return exits.success({ token: token, expires: 60000 * 10 });
     } catch (e) {
-      return exits.error(e)
+      return exits.error(e);
     }
   }
-
-}
+};
