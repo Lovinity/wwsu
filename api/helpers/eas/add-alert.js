4983b77dcef12414d7333410758b1b7702614ce1
Patrick Schmalstig <engineer@wwsu1069.org>
2021-06-18 17:11:42 -0400

    Do not execute a fetched EAS update unless changes were made to the alert.

--- api/helpers/eas/add-alert.js
+++ api/helpers/eas/add-alert.js
@@ -1,103 +1,125 @@
 module.exports = {
+  friendlyName: "eas.addAlert",
 
-  friendlyName: 'eas.addAlert',
-
-  description: 'Prepares an alert to be pushed by the eas/postParse helper.',
+  description: "Prepares an alert to be pushed by the eas/postParse helper.",
 
   inputs: {
     reference: {
-      type: 'string',
+      type: "string",
       required: true,
-      description: 'A unique ID for the alert provided by the source.'
+      description: "A unique ID for the alert provided by the source.",
     },
     source: {
-      type: 'string',
+      type: "string",
       required: true,
-      description: 'This alert originates from the provided source. Use NWS for NWS sources so that this helper can retrieve alert information.'
+      description:
+        "This alert originates from the provided source. Use NWS for NWS sources so that this helper can retrieve alert information.",
     },
     county: {
-      type: 'string',
+      type: "string",
       required: true,
-      description: 'This alert applies to the specified county.'
+      description: "This alert applies to the specified county.",
     },
     alert: {
-      type: 'string',
+      type: "string",
       required: true,
-      description: 'The alert name/event. Eg. "Severe Thunderstorm Warning".'
+      description: 'The alert name/event. Eg. "Severe Thunderstorm Warning".',
     },
     severity: {
-      type: 'string',
+      type: "string",
       required: true,
-      isIn: [ 'Extreme', 'Severe', 'Moderate', 'Minor' ],
-      description: `Severity of alert: One of the following in order from highest to lowest ['Extreme', 'Severe', 'Moderate', 'Minor']`
+      isIn: ["Extreme", "Severe", "Moderate", "Minor"],
+      description: `Severity of alert: One of the following in order from highest to lowest ['Extreme', 'Severe', 'Moderate', 'Minor']`,
     },
     starts: {
-      type: 'string',
+      type: "string",
       custom: function (value) {
-        return moment(value).isValid()
+        return moment(value).isValid();
       },
       allowNull: true,
-      description: `moment() parsable string of when the alert starts. Recommended ISO string.`
+      description: `moment() parsable string of when the alert starts. Recommended ISO string.`,
     },
     expires: {
-      type: 'string',
+      type: "string",
       custom: function (value) {
-        return moment(value).isValid()
+        return moment(value).isValid();
       },
       allowNull: true,
-      description: `moment() parsable string of when the alert expires. Recommended ISO string.`
+      description: `moment() parsable string of when the alert expires. Recommended ISO string.`,
     },
     color: {
-      type: 'string',
+      type: "string",
       regex: /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i,
-      description: 'Hex color representing this alert.',
-      required: true
+      description: "Hex color representing this alert.",
+      required: true,
     },
     information: {
-      type: 'string',
+      type: "string",
       allowNull: true,
-      description: 'Detailed information about this alert for the public.'
-    }
+      description: "Detailed information about this alert for the public.",
+    },
   },
 
   fn: async function (inputs, exits) {
-    sails.log.debug(`Helper eas.addAlert called for ${inputs.county}/${inputs.alert}.`)
+    sails.log.debug(
+      `Helper eas.addAlert called for ${inputs.county}/${inputs.alert}.`
+    );
     try {
-      var criteria = {}
+      var criteria = {};
 
       // Define a function for processing information into the sails.models.eas.pendingAlerts variable.
       // Notably, this function ensures the same alert from different counties are joined together.
       var processPending = (criteria) => {
-        if (typeof sails.models.eas.pendingAlerts[ `${inputs.source}.${inputs.reference}` ] === `undefined`) {
-          sails.models.eas.pendingAlerts[ `${inputs.source}.${inputs.reference}` ] = criteria
+        if (
+          typeof sails.models.eas.pendingAlerts[
+            `${inputs.source}.${inputs.reference}`
+          ] === `undefined`
+        ) {
+          sails.models.eas.pendingAlerts[
+            `${inputs.source}.${inputs.reference}`
+          ] = criteria;
         } else {
           for (var key in criteria) {
             if (Object.prototype.hasOwnProperty.call(criteria, key)) {
               if (key !== `counties`) {
-                sails.models.eas.pendingAlerts[ `${inputs.source}.${inputs.reference}` ][ key ] = criteria[ key ]
+                sails.models.eas.pendingAlerts[
+                  `${inputs.source}.${inputs.reference}`
+                ][key] = criteria[key];
               } else {
-                var temp = sails.models.eas.pendingAlerts[ `${inputs.source}.${inputs.reference}` ][ key ].split(', ')
-                if (temp.indexOf(inputs.county) === -1) { temp.push(inputs.county) }
-                temp = temp.join(', ')
-                sails.models.eas.pendingAlerts[ `${inputs.source}.${inputs.reference}` ][ key ] = temp
+                var temp =
+                  sails.models.eas.pendingAlerts[
+                    `${inputs.source}.${inputs.reference}`
+                  ][key].split(", ");
+                if (temp.indexOf(inputs.county) === -1) {
+                  temp.push(inputs.county);
+                }
+                temp = temp.join(", ");
+                sails.models.eas.pendingAlerts[
+                  `${inputs.source}.${inputs.reference}`
+                ][key] = temp;
               }
             }
           }
         }
-      }
+      };
 
       // Get the alert if it already exists in the database
-      var record = await sails.models.eas.findOne({ source: inputs.source, reference: inputs.reference })
+      var record = await sails.models.eas
+        .findOne({ source: inputs.source, reference: inputs.reference })
         .tolerate((err) => {
-          sails.log.error(err)
-        })
+          sails.log.error(err);
+        });
       // exists
       if (record) {
-        sails.log.verbose(`Alert ${inputs.county}/${inputs.alert} already exists.`)
+        sails.log.verbose(
+          `Alert ${inputs.county}/${inputs.alert} already exists.`
+        );
         // Detect if the county issuing the alert is already in the alert. If not, add the county in.
-        var temp = record.counties.split(', ')
-        if (temp.indexOf(inputs.county) === -1) { temp.push(inputs.county) }
-        temp = temp.join(', ')
+        var temp = record.counties.split(", ");
+        if (temp.indexOf(inputs.county) === -1) {
+          temp.push(inputs.county);
+        }
+        temp = temp.join(", ");
 
         // Prepare criteria to update
         criteria = {
@@ -107,30 +129,47 @@ module.exports = {
           severity: inputs.severity,
           color: inputs.color,
           counties: temp,
-          starts: inputs.starts !== null ? moment(inputs.starts).toISOString(true) : moment().toISOString(true),
-          expires: inputs.expires !== null ? moment(inputs.expires).toISOString(true) : moment().add(1, 'hours').toISOString(true)
+          starts:
+            inputs.starts !== null
+              ? moment(inputs.starts).toISOString(true)
+              : moment().toISOString(true),
+          expires:
+            inputs.expires !== null
+              ? moment(inputs.expires).toISOString(true)
+              : moment().add(1, "hours").toISOString(true),
+        };
+        if (
+          typeof inputs.information !== "undefined" &&
+          inputs.information !== null
+        ) {
+          criteria.information = inputs.information;
         }
-        if (typeof inputs.information !== 'undefined' && inputs.information !== null) { criteria.information = inputs.information }
 
-        sails.log.verbose(`Criteria`, criteria)
+        sails.log.verbose(`Criteria`, criteria);
 
         // Detect any changes in the alert. If a change is detected, we will do a database update.
-        var updateIt = false
+        var updateIt = false;
         for (var key in criteria) {
           if (Object.prototype.hasOwnProperty.call(criteria, key)) {
-            if (criteria[ key ] !== record[ key ]) {
-              updateIt = true
+            if (criteria[key] !== record[key]) {
+              updateIt = true;
             }
           }
         }
-        sails.log.silly(`${inputs.county}/${inputs.alert} Needs updating?: ${updateIt}`)
+        sails.log.silly(
+          `${inputs.county}/${inputs.alert} Needs updating?: ${updateIt}`
+        );
         if (updateIt) {
-          criteria._new = false
-          processPending(criteria)
+          criteria._new = false;
+          criteria._update = true;
+          processPending(criteria);
+        } else {
+          criteria._update = false;
         }
-        return exits.success()
-      } else { // Does not exist; new alert
-        sails.log.verbose(`Alert ${inputs.county}/${inputs.alert} is new.`)
+        return exits.success();
+      } else {
+        // Does not exist; new alert
+        sails.log.verbose(`Alert ${inputs.county}/${inputs.alert} is new.`);
 
         // Prepare criteria
         criteria = {
@@ -140,29 +179,51 @@ module.exports = {
           severity: inputs.severity,
           color: inputs.color,
           counties: inputs.county,
-          starts: inputs.starts !== null ? moment(inputs.starts).toISOString(true) : moment().toISOString(true),
-          expires: inputs.expires !== null ? moment(inputs.expires).toISOString(true) : moment().add(1, 'hours').toISOString(true),
-          information: inputs.information || ''
-        }
+          starts:
+            inputs.starts !== null
+              ? moment(inputs.starts).toISOString(true)
+              : moment().toISOString(true),
+          expires:
+            inputs.expires !== null
+              ? moment(inputs.expires).toISOString(true)
+              : moment().add(1, "hours").toISOString(true),
+          information: inputs.information || "",
+        };
 
         sails.log.verbose(`Initial criteria`, criteria);
 
         // If this alert came from NWS, we need to GET a separate URL for alert information before we create the record.
-        if (inputs.source === 'NWS' && (typeof sails.models.eas.pendingAlerts[ `${inputs.source}.${inputs.reference}` ] === `undefined` || sails.models.eas.pendingAlerts[ `${inputs.source}.${inputs.reference}` ].information === '' || sails.models.eas.pendingAlerts[ `${inputs.source}.${inputs.reference}` ].information === null)) {
-          sails.log.verbose('Alert is from NWS source. Retrieving alert information.')
-          var resp = await needle('get', inputs.reference)
+        if (
+          inputs.source === "NWS" &&
+          (typeof sails.models.eas.pendingAlerts[
+            `${inputs.source}.${inputs.reference}`
+          ] === `undefined` ||
+            sails.models.eas.pendingAlerts[
+              `${inputs.source}.${inputs.reference}`
+            ].information === "" ||
+            sails.models.eas.pendingAlerts[
+              `${inputs.source}.${inputs.reference}`
+            ].information === null)
+        ) {
+          sails.log.verbose(
+            "Alert is from NWS source. Retrieving alert information."
+          );
+          var resp = await needle("get", inputs.reference);
 
           // Go through each child
           var maps = resp.body.children
-            .filter(entry => typeof entry.name !== 'undefined' && entry.name === 'info')
-            .map(async entry => {
+            .filter(
+              (entry) =>
+                typeof entry.name !== "undefined" && entry.name === "info"
+            )
+            .map(async (entry) => {
               var details = {
-                description: '',
-                instruction: '',
-              }
+                description: "",
+                instruction: "",
+              };
 
               // Parse field information into the alert variable
-              entry.children.map(entry2 => {
+              entry.children.map((entry2) => {
                 switch (entry2.name) {
                   case "description":
                     details.description = entry2.value;
@@ -174,30 +235,47 @@ module.exports = {
               });
 
               // Sometimes, EAS will return "This alert has expired". If so, leave information blank.
-              if (!details.description.toLowerCase().includes('this alert has expired')) {
-                criteria.information = `${details.description === '' ? `No information is available for this alert.` : details.description}.${details.instruction !== '' ? `Precautionary / preparedness actions: ${details.instruction}.` : ``}`;
-                sails.log.verbose(`Alert ${inputs.county}/${inputs.alert} is still valid.`)
+              if (
+                !details.description
+                  .toLowerCase()
+                  .includes("this alert has expired")
+              ) {
+                criteria.information = `${
+                  details.description === ""
+                    ? `No information is available for this alert.`
+                    : details.description
+                }.${
+                  details.instruction !== ""
+                    ? `Precautionary / preparedness actions: ${details.instruction}.`
+                    : ``
+                }`;
+                sails.log.verbose(
+                  `Alert ${inputs.county}/${inputs.alert} is still valid.`
+                );
               } else {
-                sails.log.verbose(`Alert ${inputs.county}/${inputs.alert} has expired, according to NWS`)
+                sails.log.verbose(
+                  `Alert ${inputs.county}/${inputs.alert} has expired, according to NWS`
+                );
               }
-              sails.log.silly(`Criteria: ${criteria}`)
-              criteria._new = true
+              sails.log.silly(`Criteria: ${criteria}`);
+              criteria._new = true;
+              criteria._update = false;
               processPending(criteria);
-              return true
-            })
-          await Promise.all(maps)
+              return true;
+            });
+          await Promise.all(maps);
 
-          return exits.success()
+          return exits.success();
         } else {
           sails.log.verbose(`Criteria: ${criteria}`);
-          criteria._new = true
-          processPending(criteria)
-          return exits.success()
+          criteria._new = true;
+          criteria._update = false;
+          processPending(criteria);
+          return exits.success();
         }
       }
     } catch (e) {
-      return exits.error(e)
+      return exits.error(e);
     }
-  }
-
-}
+  },
+};
--- api/helpers/eas/post-parse.js
+++ api/helpers/eas/post-parse.js
@@ -1,81 +1,116 @@
 module.exports = {
+  friendlyName: "eas.postParse",
 
-  friendlyName: 'eas.postParse',
+  description: "Finalize and push out new/updated alerts.",
 
-  description: 'Finalize and push out new/updated alerts.',
-
-  inputs: {
-
-  },
+  inputs: {},
 
   fn: async function (inputs, exits) {
-    sails.log.debug('Helper eas.postParse called.')
+    sails.log.debug("Helper eas.postParse called.");
 
     try {
-      var sendit = []
+      var sendit = [];
+
+      // Get all active alerts from the database.
+      var records = await sails.models.eas.find();
+      sails.log.verbose(
+        `sails.models.eas records retrieved: ${records.length}`
+      );
+      sails.log.silly(records);
 
       // Process sails.models.eas.pendingAlerts
       for (var key in sails.models.eas.pendingAlerts) {
-        if (Object.prototype.hasOwnProperty.call(sails.models.eas.pendingAlerts, key)) {
-          sails.log.verbose(`Finalizing alert`, sails.models.eas.pendingAlerts[key])
+        if (
+          Object.prototype.hasOwnProperty.call(
+            sails.models.eas.pendingAlerts,
+            key
+          )
+        ) {
+          sails.log.verbose(
+            `Finalizing alert`,
+            sails.models.eas.pendingAlerts[key]
+          );
 
           // Do not process alerts containing no information
-          if (sails.models.eas.pendingAlerts[key].information === '' || sails.models.eas.pendingAlerts[key].information === null) {
-            sails.log.verbose(`Alert has no information. Bailing.`)
-            delete sails.models.eas.pendingAlerts[key]
-            continue
+          if (
+            sails.models.eas.pendingAlerts[key].information === "" ||
+            sails.models.eas.pendingAlerts[key].information === null
+          ) {
+            sails.log.verbose(`Alert has no information. Bailing.`);
+            delete sails.models.eas.pendingAlerts[key];
+            continue;
           }
 
           // New alerts should be created
           if (sails.models.eas.pendingAlerts[key]._new) {
-            sails.log.verbose(`Alert is new. Creating.`)
-            delete sails.models.eas.pendingAlerts[key]._new
-            await sails.models.eas.create(sails.models.eas.pendingAlerts[key]).fetch()
-            delete sails.models.eas.pendingAlerts[key]
+            sails.log.verbose(`Alert is new. Creating.`);
+            delete sails.models.eas.pendingAlerts[key]._new;
+            delete sails.models.eas.pendingAlerts[key]._update;
+            await sails.models.eas
+              .create(sails.models.eas.pendingAlerts[key])
+              .fetch();
+            delete sails.models.eas.pendingAlerts[key];
 
-            // Non-new alerts should be updated
-          } else {
-            sails.log.verbose(`Alert is old but has had changes. Updating.`)
-            delete sails.models.eas.pendingAlerts[key]._new
-            await sails.models.eas.update({ source: sails.models.eas.pendingAlerts[key].source, reference: sails.models.eas.pendingAlerts[key].reference }, sails.models.eas.pendingAlerts[key]).fetch()
-            delete sails.models.eas.pendingAlerts[key]
+            // Non-new alerts should be updated if _update is true
+          } else if (sails.models.eas.pendingAlerts[key]._update) {
+            delete sails.models.eas.pendingAlerts[key]._new;
+            delete sails.models.eas.pendingAlerts[key]._update;
+            await sails.models.eas
+              .update(
+                {
+                  source: sails.models.eas.pendingAlerts[key].source,
+                  reference: sails.models.eas.pendingAlerts[key].reference,
+                },
+                sails.models.eas.pendingAlerts[key]
+              )
+              .fetch();
+            delete sails.models.eas.pendingAlerts[key];
           }
         }
       }
 
-      // Get all active alerts from the database.
-      var records = await sails.models.eas.find()
-      sails.log.verbose(`sails.models.eas records retrieved: ${records.length}`)
-      sails.log.silly(records)
-
-      var maps = records.map(async record => {
+      var maps = records.map(async (record) => {
         // Trigger a non-fetched update of all NWS records still active so that the updatedAt is updated, and therefore the record isn't removed.
-        if (record.source === 'NWS' && sails.models.eas.activeCAPS.indexOf(record.reference) !== -1) {
-          await sails.models.eas.update({ ID: record.ID }, { updatedAt: moment().toISOString(true) })
+        if (
+          record.source === "NWS" &&
+          sails.models.eas.activeCAPS.indexOf(record.reference) !== -1
+        ) {
+          await sails.models.eas
+            .update(
+              { ID: record.ID },
+              { updatedAt: moment().toISOString(true) }
+            )
             .tolerate((err) => {
-              sails.log.error(err)
-            })
-          return true
+              sails.log.error(err);
+            });
+          return true;
         }
 
         // Remove expired alerts. Also, NWS alerts not reported by CAPS in 5 or more minutes (via updatedAt checking) should also be considered expired or canceled.
-        if ((record.source === 'NWS' && moment().isAfter(moment(record.updatedAt).add(5, 'minutes'))) || moment().isAfter(moment(record.expires))) {
-          sails.log.verbose(`Record ${record.ID} is to be deleted. It has expired.`)
-          await sails.models.eas.destroy({ ID: record.ID }).fetch()
+        if (
+          (record.source === "NWS" &&
+            moment().isAfter(moment(record.updatedAt).add(5, "minutes"))) ||
+          moment().isAfter(moment(record.expires))
+        ) {
+          sails.log.verbose(
+            `Record ${record.ID} is to be deleted. It has expired.`
+          );
+          await sails.models.eas
+            .destroy({ ID: record.ID })
+            .fetch()
             .tolerate((err) => {
-              sails.log.error(err)
-            })
-          return true
+              sails.log.error(err);
+            });
+          return true;
         }
 
-        return true
-      })
-      await Promise.all(maps)
+        return true;
+      });
+      await Promise.all(maps);
 
-      return exits.success(sendit)
+      return exits.success(sendit);
     } catch (e) {
-      return exits.error(e)
+      return exits.error(e);
     }
-  }
-
-}
+  },
+};
